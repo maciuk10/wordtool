a9baea63dd3ac9ec0a53d33d619694fc05d35b5a
Maciek Grzela <maciekgrzela45@gmail.com>
2017-09-12 08:52:34 +0200

    Form tweaks 110917

--- server/books/book.php
+++ server/books/book.php
@@ -51,6 +51,14 @@
                         <p>Strona Główna</p>
                     </a>
                 </li>
+                <li>
+                    <a href="#">
+                        <i class="pe-7s-cart">
+                            <span class="label">0</span>
+                        </i>
+                        <p class="hidden-xxs">Koszyk</p>
+                    </a>
+                </li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <i class="pe-7s-user"></i>
